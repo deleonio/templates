ae9b97e55b494d11a4753f2514650c805b346538
Martin Oppitz <github@martinoppitz.com>
2022-02-01 02:45:12 +0100

    feat: add a kolibri template

---
+++ ts-kolibri/src/App.tsx
@@ -0,0 +1,13 @@
+import { KolKolibri } from '@kolibri/solid';
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+  return (
+    <p class="text-4xl text-green-700 text-center py-20">
+      Hello <a class="text-pink-600 hover:font-bold hover:border-1" href="https://antfu.me/posts/reimagine-atomic-css" target="atomic-css">Atomic CSS</a>!
+      <KolKolibri />
+    </p>
+  );
+};
+
+export default App;
